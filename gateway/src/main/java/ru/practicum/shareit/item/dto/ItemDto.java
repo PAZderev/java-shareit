b4da1c480bd9bab8c1edfda81dbaa8bbdc114d60
PAZDerev <zdereff.pasha@ya.ru>
2024-09-27 18:35:11 +0500

    fix : Правки по комментариям  - Добавил валидацию размера в ItemDto  - Убрал лишние зависимости в модуле server

--- gateway/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.item.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.practicum.shareit.item.model.Comment;
@@ -13,8 +14,10 @@ import java.util.List;
 public class ItemDto {
     private Long id;
     @NotBlank(message = "name must be not blank")
+    @Size(max = 100)
     private String name;
     @NotNull(message = "description required")
+    @Size(max = 1000)
     private String description;
     @NotNull(message = "available field required")
     private Boolean available;
